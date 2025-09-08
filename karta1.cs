c371f905d0fd0b9b881a00f490056d3e9872a5c5
senek69 <arspashtet1@gmail.com>
2025-09-08 15:40:02 +0200

    Add files via upload

---
+++ karta1.cs
@@ -0,0 +1,51 @@
+﻿//z1
+
+////int a = int.Parse(Console.ReadLine());
+////int b = int.Parse(Console.ReadLine());
+
+////Console.WriteLine(a * a + b * b);
+//2
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine(a + b) + (a + b);
+
+//3
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine(a-b)^3);
+
+//4
+
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+//Console.WriteLine(a*b*c);
+
+
+//5
+
+//float a = float.Parse(Console.ReadLine());
+//float b = float.Parse(Console.ReadLine());
+//Console.WriteLine(2 * (a + b) / 5);
+//Console.WriteLine(a+b) * 0,4 );
+
+
+//6 
+
+//float a = float.Parse(Console.ReadLine());
+//Console.WriteLine(a/1,23);
+
+
+//7
+
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine(a%b);
+
+
+
+
+
+
+
+
